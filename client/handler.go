ddbbe6edff61ea9bcdc595170541f3038ea35310
MorselShogiew <Marsik4251@gmail.com>
2022-08-27 21:05:34 +0300

    new postgres db

---
+++ client/handler.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"io"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	lpb "github.com/MorselShogiew/UsersManager/proto/logger"
+	upb "github.com/MorselShogiew/UsersManager/proto/user"
+)
+
+var serverAddr = flag.String("serveraddr", "localhost:8001", "the address to connect to")
+var loggerAddr = flag.String("loggeraddr", "localhost:8002", "the address to connect to")
+
+func saveUser(c upb.UserRepoClient, ctx context.Context, u *upb.User) *upb.User {
+	log.Printf("Save User: {%s, %s}\n", u.GetName(), u.GetEmail())
+	saveRes, err := c.SaveUser(ctx, &upb.SaveUserRequest{Data: u})
+	if err != nil {
+		log.Fatalf("Could not save: %v", err)
+	}
+	log.Printf("Status: %s, ID: %d\n", saveRes.GetStatus(), saveRes.GetData().GetId())
+	return saveRes.GetData()
+}
+
+func deleteUser(c upb.UserRepoClient, ctx context.Context, ID int32) {
+	delRes, err := c.DeleteUser(ctx, &upb.DeleteUserRequest{Id: ID})
+	log.Printf("Delete user with ID: %d", ID)
+	if err != nil {
+		log.Fatalf("Could not delete: %v", err)
+	}
+	log.Printf("Status: %s", delRes.GetStatus())
+}
+
+func getUsers(c upb.UserRepoClient, ctx context.Context) {
+	log.Printf("Getting users")
+	stream, err := c.GetUsers(ctx, &upb.GetUsersRequest{})
+	if err != nil {
+		log.Fatalf("Could not get users: %v", err)
+	}
+	for {
+		u, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Could not get users: %v", err)
+		}
+		log.Printf("User: %d, %s, %s", u.GetId(), u.GetName(), u.GetEmail())
+	}
+}
+
+func getLogs(c lpb.LoggerRepoClient, ctx context.Context) {
+	log.Printf("Getting log")
+	logs, err := c.GetTail(ctx, &lpb.GetTailRequest{})
+	if err != nil {
+		log.Fatalf("Could not get log: %v", err)
+	}
+	for _, row := range logs.Logs {
+		log.Printf("%s: %s", row.GetTs(), row.GetMsg())
+	}
+}
+
+func main() {
+	flag.Parse()
+	time.Sleep(time.Second * 10)
+	// Set up a connection to the server.
+	sConn, err := grpc.Dial(*serverAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer sConn.Close()
+	c := upb.NewUserRepoClient(sConn)
+	log.Printf("Connected gRPC %s", *serverAddr)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	// Test saving used data
+	u := &upb.User{Name: "Ivan", Email: "example@email1"}
+	u = saveUser(c, ctx, u)
+
+	time.Sleep(time.Second)
+	log.Print("--------")
+	// If last request was more than a minute ago,
+	// we will recieve all users include last saved.
+	getUsers(c, ctx)
+
+	time.Sleep(time.Second)
+	log.Print("--------")
+	// Test saving used data.
+	u2 := &upb.User{Name: "Vasya", Email: "example@email2"}
+	u2 = saveUser(c, ctx, u2)
+
+	time.Sleep(time.Second)
+	log.Print("--------")
+	// Last user was created less then a minute ago,
+	// so we will not recieve it
+	getUsers(c, ctx)
+
+	time.Sleep(time.Second)
+	log.Print("--------")
+	// Test deleting used by gotten IDs
+	deleteUser(c, ctx, u.Id)
+	deleteUser(c, ctx, u2.Id)
+
+	// Test deleting used by unknown ID
+	deleteUser(c, ctx, 0)
+
+	time.Sleep(time.Second)
+	log.Print("--------")
+	// Set up a connection to the logger.
+	lConn, err := grpc.Dial(*loggerAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer lConn.Close()
+	l := lpb.NewLoggerRepoClient(lConn)
+	log.Printf("Connected gRPC %s", *loggerAddr)
+	// If last request was more than a minute ago,
+	// we will recieve all users include last saved.
+	ctx2, cancel2 := context.WithTimeout(context.Background(), time.Second)
+	defer cancel2()
+	getLogs(l, ctx2)
+}
---
+++ repos/user-db.go
@@ -0,0 +1,114 @@
+package repos
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	pb "github.com/MorselShogiew/UsersManager/proto/user"
+)
+
+type UserDBRepo interface {
+	SaveUser(ctx context.Context, req *pb.SaveUserRequest) (*pb.SaveUserResponse, error)
+	DeleteUser(rctx context.Context, req *pb.DeleteUserRequest) (*pb.DeleteUserResponse, error)
+	GetUsers(req *pb.GetUsersRequest, stream pb.UserRepo_GetUsersServer) error
+}
+
+type userDB struct {
+	pb.UnimplementedUserRepoServer
+	db    *DBManager
+	redis *RedisManager
+	p     *KafkaProducer
+	ctx   context.Context
+}
+
+func (s *userDB) SaveUser(ctx context.Context, req *pb.SaveUserRequest) (*pb.SaveUserResponse, error) {
+	user := req.GetData()
+	email := user.GetEmail()
+	name := user.GetName()
+	log.Printf("Save User: %v, %v", name, email)
+	var id int32
+	sqlStatement := `
+		INSERT INTO users (email, name)
+		VALUES ($1, $2)
+		RETURNING id
+	`
+	err := s.db.db.QueryRow(sqlStatement, email, name).Scan(&id)
+	if err != nil {
+		return nil, fmt.Errorf("Failed user saving: %v", err)
+	}
+	log.Printf("Created User Id: %v", id)
+	user.Id = id
+	go s.p.Produce(fmt.Sprintf("Created user with id %d, emain %s, name %s", user.Id, user.Email, user.Name))
+	return &pb.SaveUserResponse{Status: pb.Status_Success, Data: user}, nil
+}
+
+func (s *userDB) DeleteUser(ctx context.Context, req *pb.DeleteUserRequest) (*pb.DeleteUserResponse, error) {
+	id := req.GetId()
+	if id <= 0 {
+		return &pb.DeleteUserResponse{Status: pb.Status_RequestError}, nil
+	}
+	log.Printf("Delete User by id: %v", id)
+	sqlStatement := `
+		DELETE FROM users
+		WHERE id = $1;`
+	res, err := s.db.db.Exec(sqlStatement, id)
+	if err != nil {
+		return nil, err
+	}
+	c, err := res.RowsAffected()
+	if err != nil {
+		return nil, err
+	}
+	if c > 0 {
+		return &pb.DeleteUserResponse{Status: pb.Status_Success}, nil
+	}
+	return &pb.DeleteUserResponse{Status: pb.Status_NothinToDo}, nil
+}
+
+func (s *userDB) GetUsers(req *pb.GetUsersRequest, stream pb.UserRepo_GetUsersServer) error {
+	redisUsers, ok := s.redis.getUsersFromRedis()
+	if ok {
+		log.Print("Retrieve users from Redis")
+		for _, u := range *redisUsers {
+			stream.Send(&pb.User{Id: u.Id, Name: u.Name, Email: u.Email})
+		}
+		return nil
+	}
+
+	log.Print("Retrieve users from DB")
+	rows, err := s.db.db.Query("SELECT id, email, name FROM users")
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	// Loop through rows, using Scan to assign column data to struct fields.
+	var users []StoredUser
+	for rows.Next() {
+		u := &pb.User{}
+		if err := rows.Scan(&u.Id, &u.Email, &u.Name); err != nil {
+			return fmt.Errorf("Failed while scaning row: %s", err)
+		}
+		users = append(users, StoredUser{Id: u.Id, Email: u.Email, Name: u.Name})
+		stream.Send(u)
+	}
+	if err := rows.Err(); err != nil {
+		return fmt.Errorf("Failed getting users: %s", err)
+	}
+
+	log.Print("Store to Redis")
+	s.redis.storeUsersIntoRedis(&users)
+
+	return nil
+}
+
+func NewUserDB(db *DBManager, redis *RedisManager, p *KafkaProducer) *userServer {
+	s := &userDB{
+		db:    db,
+		redis: redis,
+		p:     p,
+		ctx:   context.Background(),
+	}
+	return s
+}
