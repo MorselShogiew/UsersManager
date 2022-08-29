3a5b2a593a4631ecc147bb2b076a9336b73f7949
MorselShogiew <Marsik4251@gmail.com>
2022-08-29 08:55:26 +0300

    packages

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/MorselShogiew/UsersManager/config"
 	"github.com/MorselShogiew/UsersManager/packages/color"
 	pb "github.com/MorselShogiew/UsersManager/proto/user"
+	"github.com/MorselShogiew/UsersManager/redis"
 	"github.com/MorselShogiew/UsersManager/repos"
 	"google.golang.org/grpc"
 )
@@ -21,47 +22,47 @@ func main() {
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		db.Connect(&conf.psql)
+		db.Connect(&conf.Psql)
 	}()
 
-	kafka := &KafkaProducer{}
+	kafka := &repos.KafkaProducer{}
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		kafka.Connect(&conf.kafka)
+		kafka.Connect(&conf.Kafka)
 	}()
 
-	redis := &RedisManager{}
+	redis := &redis.RedisManager{}
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		redis.Connect(&conf.redis)
+		redis.Connect(&conf.Redis)
 	}()
 
 	wg.Wait()
 
-	if db.ok {
+	if db.Ok {
 		log.Println("Connection to the DB: " + color.Green + "done" + color.Reset)
 		defer db.Close()
 	} else {
-		log.Fatalf(color.Red+"ERROR"+color.Reset+": Failed DB connection (%s:%d)", conf.psql.host, conf.psql.port)
+		log.Fatalf(color.Red+"ERROR"+color.Reset+": Failed DB connection (%s:%d)", conf.Psql.Host, conf.Psql.Port)
 	}
 
-	if kafka.ok {
+	if kafka.Ok {
 		log.Println("Connection to the Kafka: " + color.Green + "done" + color.Reset)
 		defer kafka.Close()
 	} else {
-		log.Fatalf(color.Red+"ERROR"+color.Reset+": Failed Kafka connection (%s)", conf.kafka.server)
+		log.Fatalf(color.Red+"ERROR"+color.Reset+": Failed Kafka connection (%s)", conf.Kafka.Server)
 	}
 
-	if redis.ok {
+	if redis.Ok {
 		log.Println("Connection to the Redis: " + color.Green + "done" + color.Reset)
 		defer redis.Close()
 	} else {
-		log.Fatalf(color.Red+"ERROR"+color.Reset+": Failed Redis connection (%s)", conf.redis.address)
+		log.Fatalf(color.Red+"ERROR"+color.Reset+": Failed Redis connection (%s)", conf.Redis.Address)
 	}
 
-	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", conf.grpc.port))
+	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", conf.Grpc.Port))
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
